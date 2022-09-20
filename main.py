ae885449d3e866e2b6ba9f01add778731bbc588d
Mark Pascua <markpascua680@yahoo.com>
2022-09-20 14:26:17 -0800

    Removed csv file

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from bs4 import BeautifulSoup
 import requests
  
 URL = 'https://www.amazon.com/Noctua-redux-1700-high-Performance-Award-Winning-Affordable/dp/B07CG2PGY6/ref=sr_1_1_sspa?crid=36F410L4YBBCC&keywords=pc+fan&qid=1663710594&sprefix=pc+fa%2Caps%2C201&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFDOTBQTkc5Q004MjMmZW5jcnlwdGVkSWQ9QTA4Nzc2OTcyM1RIVTRLVkM4MjlHJmVuY3J5cHRlZEFkSWQ9QTA1OTE2MjU5UExZVDU4MFRZSUYmd2lkZ2V0TmFtZT1zcF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl'
-
-File = open("out.csv", "a")
  
 HEADERS = ({'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'})
  
@@ -18,4 +16,4 @@ try:
     print(title_string)
 except AttributeError:
     title_string = "NA"
-    print("product Title = ", title_string)
+    print("Attribute could not be found")
